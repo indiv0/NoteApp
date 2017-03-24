893fd96fa2470cbb95c972888fdb6187caa67b55
Nikita Pekin <contact@nikitapek.in>
2017-03-24 15:11:51 -0400

    chore: add Joda-Time Android library
    Add the `mavenCentral` repo to the Gradle repo list.
    Add the Joda-Time Android library as a module dependency.
    Add Joda-Time Android initialization code to the `MainActivity`.

--- app/src/main/java/com/seg3125/noteapp/MainActivity.java
+++ app/src/main/java/com/seg3125/noteapp/MainActivity.java
@@ -3,11 +3,16 @@ package com.seg3125.noteapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import net.danlew.android.joda.JodaTimeAndroid;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize the Joda-Time Android library.
+        JodaTimeAndroid.init(this);
     }
 }
