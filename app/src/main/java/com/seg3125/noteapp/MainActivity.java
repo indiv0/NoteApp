6e7d6b768f5f8dcd255d202bbd4e0d2e65780a9b
Nikita Pekin <contact@nikitapek.in>
2017-03-24 16:07:56 -0400

    feat(MainActivity): add "Add Note" button
    Add "Add Note" button to the action bar.
    Add icon for "Add Note" button.
    Add code to inflate the menu actions as necessary.
    Add new action bar menu XML definition.

--- app/src/main/java/com/seg3125/noteapp/MainActivity.java
+++ app/src/main/java/com/seg3125/noteapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.seg3125.noteapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import net.danlew.android.joda.JodaTimeAndroid;
 
@@ -14,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialize the Joda-Time Android library.
         JodaTimeAndroid.init(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu, creating the necessary actions.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main_menu, menu);
+
+        return true;
     }
 }
