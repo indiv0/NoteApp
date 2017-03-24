c7b499f90bcf3158c207144b3c0e96518da2ff35
Nikita Pekin <contact@nikitapek.in>
2017-03-24 18:57:38 -0400

    impr: add "Up" button functionality
    Add "Up" button functionality to the `EditViewActivity`, for the purpose of
    returning to the parent activity.

--- app/src/main/java/com/seg3125/noteapp/EditNoteActivity.java
+++ app/src/main/java/com/seg3125/noteapp/EditNoteActivity.java
@@ -1,5 +1,6 @@
 package com.seg3125.noteapp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class EditNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_note);
+
+        // Get a support ActionBar.
+        ActionBar actionBar = getSupportActionBar();
+
+        // Enable the "Up" button to return to the previous activity.
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
